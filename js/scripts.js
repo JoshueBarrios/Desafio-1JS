accf0f88c113862d2bbbfe9da17464a970879721
Joshue Barrios <joshuedavidbarrios@gmail.com>
2022-02-26 00:53:53 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//debugger
+/* let entrada = prompt("Ingresa tu nombre");
+let mensaje1 = "hola"
+let mensaje = "bienvenido a mi web de javascript";
+let resultado =mensaje1 +" " +entrada +" "+ mensaje;
+alert(resultado); */
+
+let num1 , num2;
+
+num1 = Number(prompt("Ingrese tu primer numero a sumar")) ;
+
+num2 = Number (prompt ("ingrese tu segundo numero a sumar"));
+
+
+let resultado = num1 + num2;
+
+alert (resultado);
+
+
+//parseInt --> 50
+//parseFloat --> 50.6
